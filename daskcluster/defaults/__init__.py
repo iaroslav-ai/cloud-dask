6311bcf87478db9049d4f880c95f9dcbaf56fbe4
Iaroslav Shcherbatyi <iaroslav.email@gmail.com>
2018-02-12 00:13:58 +0100

    Clean up a bit the names

--- daskcluster/defaults/__init__.py
+++ daskcluster/defaults/__init__.py
@@ -3,8 +3,8 @@ ec2_defaults = {
     "target_capacity": 2,
     "instance_type": "m3.medium",
     "ami_image_id": "ami-ba5dd8d5",
-    "aws_access_key": "/home/iaroslav/iaroslav-frankfurt-ec.pem",
-    "aws_key_name": "iaroslav-frankfurt-ec",
+    "aws_access_key": "/home/user/aws-access-key.pem",
+    "aws_key_name": "aws-access-key",
     "security_group": "sg-7996a612",
     "iamfleetrole": "arn:aws:iam::761799855167:role/aws-ec2-spot-fleet-tagging-role"
 }
